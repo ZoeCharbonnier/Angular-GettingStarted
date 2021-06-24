179dd505e68fd243f817d3a9a3fd90a24ae72ac4
ZoeCharbonnierPXL <zoe.charbonnier@student.pxl.be>
2021-06-24 21:09:54 +0200

    templates, interpolation, directives

--- APM-New/src/app/app.component.ts
+++ APM-New/src/app/app.component.ts
@@ -5,7 +5,7 @@ import { Component } from '@angular/core';
   template: `
     <div>
       <h1>{{ pageTitle }}</h1>
-      <p>My First Component</p>
+      <pm-product-list></pm-product-list>
     </div>
   `,
 })
