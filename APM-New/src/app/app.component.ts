e0b3c78b46fae5ed5883af8abdc1d1a198f35364
ZoeCharbonnierPXL <zoe.charbonnier@student.pxl.be>
2021-06-24 20:27:11 +0200

    components

--- APM-New/src/app/app.component.ts
+++ APM-New/src/app/app.component.ts
@@ -2,9 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'pm-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <div>
+      <h1>{{ pageTitle }}</h1>
+      <p>My First Component</p>
+    </div>
+  `,
 })
 export class AppComponent {
-  title = 'APM-New';
+  pageTitle: string = 'Product Management';
 }
